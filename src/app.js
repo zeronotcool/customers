4c4278cab0d6e7a7dcacd8a274bf9e38f74cf3dd
ZeroNotCool <zeronotcool@nomail.com>
2023-11-22 00:50:23 +0100

    Cambiando a ejemplo con Express

--- src/app.js
+++ src/app.js
@@ -11,6 +11,24 @@ import { v4 as uuidv4 } from 'uuid';
 console.log(uuidv4());
 */
 
+/*
 const { v4: uuidv4 } = require('uuid');
 console.log(uuidv4());
 console.log('panete santo');
+*/
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('This is a GET request');    
+});
+
+app.post('/', (req, res) => {
+    res.send('This is a POST request');    
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+});
